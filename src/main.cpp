824caaef9f640b0d7fb11d11665bacfde54f973d
Nikmoofte <ilya.vailov@gmail.com>
2023-12-05 23:54:03 +0300

    text file input

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
-#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <string>
 
 #include "stringSortingQueue/stringSortingQueue.hpp"
 #include "factory/factory.hpp"
 
 int main()
 {
-    
+    using namespace std;
+    fstream in("../src/assets/text.txt");
+    istream_iterator<char> iter(in);
+    std::string text(iter, {});
+
+
+
+
     return 0;
 }
